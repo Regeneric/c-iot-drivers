8988f2c83a075fc1cab0129c957005ee0f20b64a
Regeneric <hubek916@gmail.com>
2026-07-10 16:37:03 +0200

    SGP30 calibration

--- src/platform/rp2350/utils/utils.cpp
+++ src/platform/rp2350/utils/utils.cpp
@@ -10,13 +10,15 @@ void sleep_us(uint64 us) {::sleep_us(us);}
 void sleep_ms(uint64 ms) {::sleep_ms(ms);}
 
 bool8 repeating_timer_us(int64 us, void *callback, void *data) {
-    static repeating_timer_t rt;
-    return ::add_repeating_timer_us(us, static_cast<repeating_timer_callback_t>(callback), data, &rt);
+    // static repeating_timer_t rt;
+    // return ::add_repeating_timer_us(us, static_cast<repeating_timer_callback_t>(callback), data, &rt);
+    return true;
 }
 
 bool8 repeating_timer_ms(int64 ms, void *callback, void *data) {
-    static repeating_timer_t rt;
-    return ::add_repeating_timer_ms(ms, static_cast<repeating_timer_callback_t>(callback), data, &rt);
+    // static repeating_timer_t rt;
+    // return ::add_repeating_timer_ms(ms, static_cast<repeating_timer_callback_t>(callback), data, &rt);
+    return true;
 }
 
 }
