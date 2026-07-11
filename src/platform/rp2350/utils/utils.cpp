ec00ddf01e893003763a1b540e46c76b0e64d522
Regeneric <hubek916@gmail.com>
2026-07-11 03:01:27 +0200

    SGP30 calibration

--- src/platform/rp2350/utils/utils.cpp
+++ src/platform/rp2350/utils/utils.cpp
@@ -11,23 +11,22 @@ namespace hkk::utils {
 void sleep_us(uint64 us) {::sleep_us(us);}
 void sleep_ms(uint64 ms) {::sleep_ms(ms);}
 
+static repeating_timer_t rt1;
+static repeating_timer_t rt2;
+
 bool8 repeating_timer_us(int64 us, void *callback, void *data) {
     HTRACE("utils.cpp -> repeating_timer_us(int64, void*, void*):bool8");
     
-    static repeating_timer_t rt;
     auto cb = (repeating_timer_callback_t)(callback);
-
-    return ::add_repeating_timer_us(us, cb, data, &rt);
+    return ::add_repeating_timer_us(us, cb, data, &rt1);
 
 }
 
 bool8 repeating_timer_ms(int64 ms, void *callback, void *data) {
     HTRACE("utils.cpp -> repeating_timer_ms(int64, void*, void*):bool8");
 
-    static repeating_timer_t rt;
     auto cb = (repeating_timer_callback_t)(callback);
-
-    return ::add_repeating_timer_ms(ms, cb, data, &rt);
+    return ::add_repeating_timer_ms(ms, cb, data, &rt2);
 }
 
 bool8 alarm_us(uint32 us, void *callback, void *data, bool8 fire_if_past) {
